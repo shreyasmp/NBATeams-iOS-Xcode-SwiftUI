ecad34e6e2ede58b7a9eee447107414d9ea103ea
Shreyas Muthkur <shreyasspidy@gmail.com>
2025-01-16 00:05:08 -0500

    Initial Commit

---
+++ NBATeams/NBATeamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBATeamsApp.swift
+//  NBATeams
+//
+//  Created by Shreyas Muthkur on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct NBATeamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
